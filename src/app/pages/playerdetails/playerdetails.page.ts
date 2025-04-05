e8b76472d22c5ffbe0015778d055b9d4c6f556cc
Sammy <svmmydev@gmail.com>
2025-04-06 01:59:39 +0200

    (Quick Fix, implementation) Getting playerId on playerdetails from playerlist

--- src/app/pages/playerdetails/playerdetails.page.ts
+++ src/app/pages/playerdetails/playerdetails.page.ts
@@ -1,18 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonCard, IonButtons, IonButton, IonIcon, IonList, IonCardHeader, IonCardTitle, IonItem, IonCardSubtitle } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonCard, IonButtons, IonButton, IonIcon, IonList, IonCardHeader, IonCardTitle, IonItem, IonCardSubtitle, IonCardContent, IonNote } from '@ionic/angular/standalone';
+import { ActivatedRoute, Route } from '@angular/router';
+import { ApiService } from 'src/app/services/api.service';
+import { Player } from 'src/app/models/player.model';
+import { Playerlist } from '../playerlist/playerlist.page';
 
 @Component({
   selector: 'app-playerdetails',
   templateUrl: './playerdetails.page.html',
   styleUrls: ['./playerdetails.page.scss'],
-  imports: [IonCardSubtitle, IonCardTitle, IonCardHeader, IonIcon, IonButton, IonButtons, IonCard, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonNote, IonCardContent, IonCardSubtitle, IonCardTitle, IonCardHeader, IonIcon, IonButton, IonButtons, IonCard, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class PlayerdetailsPage implements OnInit {
+  player?: Player;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private apiService: ApiService) {}
+  
 
-  ngOnInit() { }
-
+  ngOnInit() { 
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.player = this.apiService.players.find(p => p.id === id);
+  }
 }
