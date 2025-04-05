cda7bcb672683572e563af1dbb44815952ead91f
Sammy <svmmydev@gmail.com>
2025-04-05 18:07:17 +0200

    Playerdetails page implemented

--- src/app/pages/playerlist/playerlist.page.ts
+++ src/app/pages/playerlist/playerlist.page.ts
@@ -1,21 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonItem, IonButton, IonList, IonLabel, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonIcon, IonButtons } from '@ionic/angular/standalone';
-import { addIcons } from 'ionicons';
-import { camera, shareSocial } from 'ionicons/icons';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonList, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonIcon, IonButtons } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-playerlist',
   templateUrl: './playerlist.page.html',
   styleUrls: ['./playerlist.page.scss'],
   standalone: true,
-  imports: [IonButtons, IonIcon, IonCardContent, IonCardSubtitle, IonCardTitle, IonCardHeader, IonCard, IonLabel, IonList, IonButton, IonItem, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonButtons, IonIcon, IonCardContent, IonCardSubtitle, IonCardTitle, IonCardHeader, IonCard, IonList, IonButton, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class Playerlist implements OnInit {
 
   constructor() {
-      addIcons({camera,shareSocial});
   }
 
   ngOnInit() {
