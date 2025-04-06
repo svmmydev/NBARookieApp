822904f03c24b0085c8e6cbbd022d6c3ea464f4d
Sammy <svmmydev@gmail.com>
2025-04-06 18:52:28 +0200

    Local auth to firebase Auth

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAXvJ5pNG-wzLDP6LDHwRlL0osWyb7Wv9Q",
+    authDomain: "nbarookieapp-fad2e.firebaseapp.com",
+    projectId: "nbarookieapp-fad2e",
+    storageBucket: "nbarookieapp-fad2e.firebasestorage.app",
+    messagingSenderId: "133109009732",
+    appId: "1:133109009732:web:95c6c75500697947b0db11",
+    measurementId: "G-5JQRYY9MGY"
+  }
 };
